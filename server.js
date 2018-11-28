2c3c58f071455b2617756e79549fdea6bfae65fd
Michael Peterson <WebDev@ctoms.ca>
2018-11-28 10:11:43 -0700

    set up initial server

--- server.js
+++ server.js
@@ -1,1 +1,42 @@
 require('dotenv').config();
+const express = require('express');
+const {PORT} = require('./config');
+const app = express();
+
+let server;
+
+function runServer(databaseUrl, port = PORT) {
+
+  return new Promise((resolve, reject) => {
+   
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
